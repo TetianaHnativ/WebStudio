c7680c3227a49117547ee2a16e63d0fafe67b80f
LightDeny <kozak.denusko@gmail.com>
2023-04-01 13:51:29 +0300

    add library mongose for connect mongodb

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,14 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
 
 const PORT = 5000;
 
 const app = express();
 
-app.get('/', (req, res) => 
-{
+app.use(express.json());
+
+app.post('/', (req, res) => {
+    console.log(req.body);
     res.status(200).json('Server work');
 })
 
