063dc864d4c1c18f5749c4b357de5c0551cddeaa
LightDeny <kozak.denusko@gmail.com>
2023-04-04 17:42:05 +0300

    Preparing for rest api architecture

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const Contact = require('./contact.js');
+const Contact = require('./model/contact.js');
 
 const PORT = 5000;
 const DB_URL = `mongodb+srv://localhost:root@webstudio.4yjossz.mongodb.net/?retryWrites=true&w=majority`;
@@ -11,14 +11,14 @@ const DB_URL = `mongodb+srv://localhost:root@webstudio.4yjossz.mongodb.net/?retr
 const app = express();
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 app.get('/portfolio', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'portfolio.html'));
 });
 
 app.post('/', async (req, res) => {
@@ -33,7 +33,7 @@ app.post('/', async (req, res) => {
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'public', 'error.html'));
+  res.status(404).sendFile(path.join(__dirname, '..', 'public', 'error.html'));
 });
 
 async function startApp() {
