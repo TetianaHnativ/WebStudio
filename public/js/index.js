b2cc2fc7a736756b65a2cbfe438e36a9603b62fa
LightDeny <kozak.denusko@gmail.com>
2023-05-20 13:56:42 +0300

    Implementation of the logout button

--- public/js/index.js
+++ public/js/index.js
@@ -10,3 +10,33 @@ serviceSelect.innerHTML = services
     .map((service) => `<option value="${service._id}">${service.name}</option>`)
     .join('');
 });
+
+const logoutButton = document.getElementById('logoutButton');
+
+if(logoutButton) {
+    logoutButton.addEventListener('click', async (event) => {
+    event.preventDefault();
+
+    try {
+        const response = await fetch('/auth/logout', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'same-origin'
+        });
+
+        if (response.ok) {
+            // Видалення кукі refreshToken
+            document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+            // Виконайте будь-які інші дії, необхідні після виходу з системи
+            console.log('Logged out successfully');
+            window.location.href = "/";
+        } else {
+            console.log('Logout failed');
+        }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    });
+}
